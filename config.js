d071e761a484e217749725d67189571f1024d040
giftedsession <oduorid606@gmail.com>
2025-07-13 03:50:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,80 +4,145 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-// add your Session Id 
-AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-// make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-// make true if you want auto reply on status 
-AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-// make true if you want auto reply on status 
-AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY KHAN-MD 🤍*",
-// set the auto reply massage on status reply  
-WELCOME: process.env.WELCOME || "true",
-// true if want welcome and goodbye msg in groups    
-ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-// make true to know who dismiss or promoted a member in group
-ANTI_LINK: process.env.ANTI_LINK || "true",
-// make anti link true,false for groups 
-MENTION_REPLY: process.env.MENTION_REPLY || "false",
-// make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/7zfdcq.jpg",
-// add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || ".",
-// add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "KHAN-MD",
-// add bot namw here for menu
-STICKER_NAME: process.env.STICKER_NAME || "KHAN-MD",
-// type sticker pack name 
-CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-// make this true for custum emoji react    
-CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,🧡,💛,💚,💙,💜,🤎,🖤,🤍",
-// chose custom react emojis by yourself 
-DELETE_LINKS: process.env.DELETE_LINKS || "false",
-// automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "92342758XXXX",
-// add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "Jᴀᴡᴀᴅ TᴇᴄʜX",
-// add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX*",
-// add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/149k8x.jpg",
-// add img for alive msg
-LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *KHAN-MD*⚡",
-// add alive msg here 
-READ_MESSAGE: process.env.READ_MESSAGE || "false",
-// Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "false",
-// make this true or false for auto react on all msgs
-ANTI_BAD: process.env.ANTI_BAD || "false",
-// false or true for anti bad words  
-MODE: process.env.MODE || "public",
-// make bot public-private-inbox-group 
-ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-// make anti link true,false for groups 
-AUTO_VOICE: process.env.AUTO_VOICE || "false",
-// make true for send automatic voices
-AUTO_STICKER: process.env.AUTO_STICKER || "false",
-// make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "false",
-// make true or false automatic text reply 
-ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-// maks true for always online 
-PUBLIC_MODE: process.env.PUBLIC_MODE || "true",
-// make false if want private mod
-AUTO_TYPING: process.env.AUTO_TYPING || "false",
-// true for automatic show typing   
-READ_CMD: process.env.READ_CMD || "false",
-// true if want mark commands as read 
-DEV: process.env.DEV || "923427582273",
-//replace with your whatsapp number        
-ANTI_VV: process.env.ANTI_VV || "true",
-// true for anti once view 
-ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "log", 
-// change it to 'same' if you want to resend deleted message in same chat 
-AUTO_RECORDING: process.env.AUTO_RECORDING || "false"
-// make it true for auto recoding 
+    SESSION_ID: process.env.SESSION_ID || "",
+    // add your Session Id make sure it starts with Gifted~
+
+    PREFIX: process.env.PREFIX || ".",
+    // add your prefix for bot
+
+    BOT_NAME: process.env.BOT_NAME || "DAVE-MD",
+    // add bot name here for menu
+
+    MODE: process.env.MODE || "public",
+    // make bot public-private-inbox-group 
+
+    LINK_WHITELIST: "youtube.com,github.com",
+
+    LINK_WARN_LIMIT: 3, // Number of warnings before action
+
+    LINK_ACTION: "kick", // "kick", "mute", or "none"
+
+    AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
+    // make true or false status auto seen
+
+    AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
+    // make true if you want auto reply on status 
+
+    AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
+    // make true if you want auto reply on status 
+
+    AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DAVE-MD 😆*",
+    // set the auto reply message on status reply  
+
+    WELCOME: process.env.WELCOME || "true",
+    // true if want welcome and goodbye msg in groups 
+
+    ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
+    // make true to know who dismiss or promoted a member in group
+
+    ANTI_LINK: process.env.ANTI_LINK || "true",
+    // make anti link true,false for groups 
+
+    MENTION_REPLY: process.env.MENTION_REPLY || "false",
+    // make true if want auto voice reply if someone mention you 
+
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/nxzaly.jpg",
+    // add custom menu and mention reply image url
+
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/nxzaly.jpg",
+    // add img for alive msg
+
+    LIVE_MSG: process.env.LIVE_MSG || 
+`> ʙᴏᴛ ɪs sᴘᴀʀᴋɪɴɢ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ᴀʟɪᴠᴇ
+
+ᴋᴇᴇᴘ ᴜsɪɴɢ ✦DAVE-MD✦ ғʀᴏᴍ DAVE ᴛᴇᴄʜ ɪɴᴄ⚡
+
+*© ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ*
+
+> ɢɪᴛʜᴜʙ : github.com/giftedsession/DAVE-MD`,
+    // add alive msg here 
+
+    STICKER_NAME: process.env.STICKER_NAME || "DAVE-MD",
+    // type sticker pack name 
+
+    CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
+    // make this true for custom emoji react  
+
+    CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,💛,💚,💙,💜,🤎,🖤,🤍",
+    // choose custom react emojis by yourself 
+
+    DELETE_LINKS: process.env.DELETE_LINKS || "false",
+    // automatic delete links without removing member 
+
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "26371475XXXX",
+    // add your bot owner number
+
+    OWNER_NAME: process.env.OWNER_NAME || " GIFTED DAVE ",
+    // add bot owner name
+
+    DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ GIFTED DAVE*",
+    // add bot owner description  
+
+    READ_MESSAGE: process.env.READ_MESSAGE || "false",
+    // Turn true or false for automatic read msgs
+
+    AUTO_REACT: process.env.AUTO_REACT || "false",
+    // make this true or false for auto react on all msgs
+
+    ANTI_BAD: process.env.ANTI_BAD || "false",
+    // false or true for anti bad words  
+
+    ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
+    // make anti link true,false for groups 
+
+    AUTO_STICKER: process.env.AUTO_STICKER || "false",
+    // make true for automatic stickers 
+
+    AUTO_REPLY: process.env.AUTO_REPLY || "false",
+    // make true or false automatic text reply 
+
+    ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
+    // make true for always online 
+
+    PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
+    // make false if want private mode
+
+    AUTO_TYPING: process.env.AUTO_TYPING || "false",
+    // true for automatic show typing  
+
+    READ_CMD: process.env.READ_CMD || "false",
+    // true if want mark commands as read  
+
+    DEV: process.env.DEV || "254104260236",
+    // replace with your whatsapp number  
+
+    ANTI_VV: process.env.ANTI_VV || "true",
+    // true for anti once view 
+
+    ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
+    // change it to 'inbox' or 'same' if you want to resend deleted message in same chat 
+
+    AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
+    // make it true for auto recording 
+
+    version: process.env.version || "2.0.0",
+
+    START_MSG: process.env.START_MSG || 
+`*Hᴇʟʟᴏ ᴛʜᴇʀᴇ DAVE-MD ᴄᴏɴɴᴇᴄᴛᴇᴅ! 👋🏻* 
+
+*ᴋᴇᴇᴘ ᴏɴ ᴜsɪɴɢ DAVE-MD 🚩* 
+
+> sᴜʙsᴄʀɪʙᴇ ʏᴛ ᴄʜᴀɴɴᴇʟ ғᴏʀ ᴛᴜᴛᴏʀɪᴀʟs
+https://youtube.com/@davlodavlo19
+
+- *ʏᴏᴜʀ ʙᴏᴛ ᴘʀᴇғɪx: ➡️[ . ]*
+> - ʏᴏᴜ ᴄᴀɴ ᴄʜᴀɴɢᴇ ᴜʀ ᴘʀᴇғɪx ᴜsɪɴɢ ᴛʜᴇ .ᴘʀᴇғɪx ᴄᴏᴍᴍᴀɴᴅ
+
+> ᴅᴏɴᴛ ғᴏʀɢᴇᴛ ᴛᴏ sʜᴀʀᴇ, sᴛᴀʀ & ғᴏʀᴋ ᴛʜᴇ ʀᴇᴘᴏ ⬇️ 
+https://github.com/giftedsession/DAVE-MD
+
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ GIFTED DAVE 🇿🇼`
 };
-  
+    
