4377564c3931f5288349f300c5a7930152befff0
gifteddevsmd <babydave081@gmail.com>
2025-07-15 20:38:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-module.exports = {
+let config = {
     SESSION_ID: process.env.SESSION_ID || "",
     // add your Session Id make sure it starts with Gifted~
 
@@ -25,34 +25,17 @@ module.exports = {
     LINK_ACTION: "kick", // "kick", "mute", or "none"
 
     AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
-    // make true or false status auto seen
-
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
-    // make true if you want auto reply on status 
-
     AUTO_STATUS_REACT: process.env.AUTO_STATUS_REACT || "true",
-    // make true if you want auto reply on status 
-
     AUTO_STATUS_MSG: process.env.AUTO_STATUS_MSG || "*SEEN YOUR STATUS BY DAVE-MD 😆*",
-    // set the auto reply message on status reply  
 
     WELCOME: process.env.WELCOME || "true",
-    // true if want welcome and goodbye msg in groups 
-
     ADMIN_EVENTS: process.env.ADMIN_EVENTS || "false",
-    // make true to know who dismiss or promoted a member in group
-
     ANTI_LINK: process.env.ANTI_LINK || "true",
-    // make anti link true,false for groups 
-
     MENTION_REPLY: process.env.MENTION_REPLY || "false",
-    // make true if want auto voice reply if someone mention you 
 
-    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/nxzaly.jpg",
-    // add custom menu and mention reply image url
-
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/nxzaly.jpg",
-    // add img for alive msg
+    MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/vr83h2.jpg",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/vr83h2.jpg",
 
     LIVE_MSG: process.env.LIVE_MSG || 
 `> ʙᴏᴛ ɪs sᴘᴀʀᴋɪɴɢ ᴀᴄᴛɪᴠᴇ ᴀɴᴅ ᴀʟɪᴠᴇ
@@ -62,87 +45,46 @@ module.exports = {
 *© ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ - ᴍᴅ*
 
 > ɢɪᴛʜᴜʙ : github.com/giftedsession/DAVE-MD`,
-    // add alive msg here 
 
     STICKER_NAME: process.env.STICKER_NAME || "DAVE-MD",
-    // type sticker pack name 
 
     CUSTOM_REACT: process.env.CUSTOM_REACT || "false",
-    // make this true for custom emoji react  
-
     CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🩹,❤️,💛,💚,💙,💜,🤎,🖤,🤍",
-    // choose custom react emojis by yourself 
 
     DELETE_LINKS: process.env.DELETE_LINKS || "false",
-    // automatic delete links without removing member 
 
-    OWNER_NUMBER: process.env.OWNER_NUMBER || "26371475XXXX",
-    // add your bot owner number
-
+    OWNER_NUMBER: process.env.OWNER_NUMBER || "2541042XXXX",
     OWNER_NAME: process.env.OWNER_NAME || " GIFTED DAVE ",
-    // add bot owner name
-
     DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ GIFTED DAVE*",
-    // add bot owner description  
 
     READ_MESSAGE: process.env.READ_MESSAGE || "false",
-    // Turn true or false for automatic read msgs
-
     AUTO_REACT: process.env.AUTO_REACT || "false",
-    // make this true or false for auto react on all msgs
-
     ANTI_BAD: process.env.ANTI_BAD || "false",
-    // false or true for anti bad words  
-
     ANTI_LINK_KICK: process.env.ANTI_LINK_KICK || "false",
-    // make anti link true,false for groups 
-
     AUTO_STICKER: process.env.AUTO_STICKER || "false",
-    // make true for automatic stickers 
-
     AUTO_REPLY: process.env.AUTO_REPLY || "false",
-    // make true or false automatic text reply 
-
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
-    // make true for always online 
-
     PUBLIC_MODE: process.env.PUBLIC_MODE || "false",
-    // make false if want private mode
-
     AUTO_TYPING: process.env.AUTO_TYPING || "false",
-    // true for automatic show typing  
-
     READ_CMD: process.env.READ_CMD || "false",
-    // true if want mark commands as read  
 
     DEV: process.env.DEV || "254104260236",
-    // replace with your whatsapp number  
 
     ANTI_VV: process.env.ANTI_VV || "true",
-    // true for anti once view 
-
     ANTI_DEL_PATH: process.env.ANTI_DEL_PATH || "inbox",
-    // change it to 'inbox' or 'same' if you want to resend deleted message in same chat 
-
     AUTO_RECORDING: process.env.AUTO_RECORDING || "false",
-    // make it true for auto recording 
 
     version: process.env.version || "2.0.0",
 
-    START_MSG: process.env.START_MSG || 
-`*Hᴇʟʟᴏ ᴛʜᴇʀᴇ DAVE-MD ᴄᴏɴɴᴇᴄᴛᴇᴅ! 👋🏻* 
-
-*ᴋᴇᴇᴘ ᴏɴ ᴜsɪɴɢ DAVE-MD 🚩* 
-
-> sᴜʙsᴄʀɪʙᴇ ʏᴛ ᴄʜᴀɴɴᴇʟ ғᴏʀ ᴛᴜᴛᴏʀɪᴀʟs
-https://youtube.com/@davlodavlo19
-
-- *ʏᴏᴜʀ ʙᴏᴛ ᴘʀᴇғɪx: ➡️[ . ]*
-> - ʏᴏᴜ ᴄᴀɴ ᴄʜᴀɴɢᴇ ᴜʀ ᴘʀᴇғɪx ᴜsɪɴɢ ᴛʜᴇ .ᴘʀᴇғɪx ᴄᴏᴍᴍᴀɴᴅ
-
-> ᴅᴏɴᴛ ғᴏʀɢᴇᴛ ᴛᴏ sʜᴀʀᴇ, sᴛᴀʀ & ғᴏʀᴋ ᴛʜᴇ ʀᴇᴘᴏ ⬇️ 
-https://github.com/giftedsession/DAVE-MD
-
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ GIFTED DAVE 🇿🇼`
+    START_MSG: process.env.START_MSG || "> Bot started successfully ✅"
 };
-    
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+    fs.unwatchFile(file);
+    console.log(`🔥 Update detected in '${__filename}', reloading config...`);
+    delete require.cache[file];
+    config = require(file);
+});
+
+module.exports = config;
